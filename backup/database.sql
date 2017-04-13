e1732504bfd121b900b30a292b00f596d532df70
Alex Kua <alexkua@me.com>
2017-04-12 23:11:16 -0400

    Re-manage db files

--- backup/database.sql
+++ backup/database.sql
@@ -140,18 +140,3 @@ recommendations
 cle_magiques
 
 */
-
-CREATE TABLE `ak_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `firstname` varchar(100) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `salt` varchar(100) NOT NULL,
-  `telephone` varchar(45) NOT NULL,
-  `role` int(11) NOT NULL DEFAULT '1' COMMENT 'Role 1: Super admin\nRole 2: Admin\nRole 3: Editor/Regular user',
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `profile_pic` varchar(300) NOT NULL DEFAULT 'resources/img/default-profile.png',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- db/seeder.sql
+++ db/seeder.sql
@@ -3,5 +3,5 @@
  */
 
 /* ak_users */
-TRUNCABLE TABLE `alexkua`.`ak_users`;
+TRUNCATE TABLE `alexkua`.`ak_users`;
 INSERT INTO `alexkua`.`ak_users` (`id`, `email`, `firstname`, `lastname`, `password`, `salt`, `telephone`, `role`, `active`, `profile_pic`) VALUES ('1', 'alexkua@me.com', 'Alex', 'Kua', '621ff7c8e618db8fab41a753992a90e0', '5a2bab2e17b0cb65887882a601cb7a6f4b38e635aaef', '(438) 870-0376', '1', '1', 'resources/img/default-profile.png');
